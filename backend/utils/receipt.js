ebfee4ededc8ae479b6948e78c3f651853850c2a
BlazerHeat <vghoghari82@gmail.com>
2023-02-06 15:24:03 +0530

    update

--- backend/utils/receipt.js
+++ backend/utils/receipt.js
@@ -2,30 +2,20 @@ const pdf = require('html-pdf');
 const ejs = require('ejs');
 const fs = require('fs');
 
+
 const html = fs.readFileSync('receipt_template.ejs', 'utf8');
-const data = {
-    username: 'Vatsal'
-};
 
-const renderedHtml = ejs.render(html, data);
-
-pdf.create(renderedHtml).toFile('./receipt.pdf', function (err, res) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(res);
-    }
-});
-
-
-const generateReceipt = (id, name, amount) => {
-    const renderedHtml = ejs.render(html, { id, name, amount });
-
-    pdf.create(renderedHtml).toFile('./receipt.pdf', function (err, res) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(res);
-        }
+const generateReceiptBuffer = ({ name, amount, email, phone }) => {
+    return new Promise((resolve, reject) => {
+        const renderedHtml = ejs.render(html, { name, amount, email, phone });
+        pdf.create(renderedHtml).toBuffer((err, buffer) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(buffer);
+            }
+        });
     });
 };
+
+module.exports = { generateReceiptBuffer };
