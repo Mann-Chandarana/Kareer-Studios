5e5bc76a29473e63f2fc2b0f75d92dc4a5cd5fa7
BlazerHeat <vghoghari82@gmail.com>
2023-02-07 10:12:46 +0530

    update

--- backend/utils/otpGen.js
+++ backend/utils/otpGen.js
@@ -1,4 +1,9 @@
-module.exports = () => {
-    let otp = ((1000+(Math.floor(Math.random() * 10000))).toString()).substr(0,4);
+const generateOtp = (length = 4) => {
+    let otp = "";
+    for (let i = 0; i < length; i++) {
+        otp += Math.floor(Math.random() * 10);
+    }
     return otp;
-};;
+};
+
+module.exports = generateOtp;
