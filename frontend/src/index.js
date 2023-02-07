a77e108f169edf0721bc02c172d00c75eea0e167
BlazerHeat <vghoghari82@gmail.com>
2023-02-07 12:23:06 +0530

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-
-ReactDom.render(<App />,document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
