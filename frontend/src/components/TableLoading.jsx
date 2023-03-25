e11b6d08bd0741613581a3cbed01579673161458
Vatsal Ghoghari <vghoghari82@gmail.com>
2023-03-25 13:08:48 +0530

    Update

--- frontend/src/components/TableLoading.jsx
+++ frontend/src/components/TableLoading.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export const TableLoading = () => {
     return (
         <tr className="w-100 d-flex justify-content-center border-0">
-            <t className="border-0">
+            <td className="border-0">
                 <div className="spinner-border" role="status" style={{ color: '#4154f1' }}>
                     <span className="visually-hidden">Loading...</span>
                 </div>
-            </t>
+            </td>
         </tr>
     );
 };
