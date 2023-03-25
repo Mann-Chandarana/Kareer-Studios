a55e2883f8f49984934d5fb4a966fcd3405d18aa
Vatsal Ghoghari <vghoghari82@gmail.com>
2023-03-25 19:42:16 +0530

    Update

--- frontend/src/components/tables/students/AddParent.jsx
+++ frontend/src/components/tables/students/AddParent.jsx
@@ -1,8 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import client from '../../../api';
 import SessionContext from '../../../contexts/SessionContext';
-import '../../../styles/addparent.css';
 
 function AddParent() {
     const { user, renewUser } = useContext(SessionContext);
@@ -13,8 +12,10 @@ function AddParent() {
         name: '',
         email: '',
         phone: '',
-        gender: '',
+        gender: 'father',
     });
+
+    const formRef = useRef();
 
     if (user.parent_added) {
         navigate('/');
@@ -32,8 +33,14 @@ function AddParent() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        formRef.current.classList.add('was-validated');
+        if (!formRef.current.checkValidity()) {
+            return;
+        }
+
         parent.student_id = user.id;
-        console.log(parent);
+
         try {
             await client.post('/parents', parent);
             setParent({
@@ -43,6 +50,7 @@ function AddParent() {
                 gender: '',
             });
             await renewUser();
+            formRef.current.classList.remove('was-validated');
             navigate('/parent');
         } catch (error) {
             console.log({ error: error });
@@ -52,78 +60,104 @@ function AddParent() {
     return (
         <main id="main" className="d-flex align-items-center">
             <div className="container">
-                <form onSubmit={handleSubmit}>
-                    <div className="row">
-                        <h4 style={{ margin: '1rem 0', fontSize: '2rem' }}>Add parent</h4>
-                        <div className="input-group input-group-icon">
+                <h4 style={{ margin: '1rem 0', fontSize: '2rem' }}>Add Parent</h4>
+                <form className="d-flex flex-column gap-2" onSubmit={handleSubmit} ref={formRef} noValidate>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">
+                            <i className="fa fa-user icon" style={{ marginRight: '10px' }}></i>
+                        </span>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="name"
+                            pattern="^[a-z A-Z]*$"
+                            value={parent.name}
+                            placeholder="Enter your parent name"
+                            onChange={handleChange}
+                            required
+                        />
+                        <div className="invalid-feedback">Please enter a valid username.</div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">
+                            <i className="fa fa-envelope" style={{ marginRight: '10px' }}></i>
+                        </span>
+                        <input
+                            type="email"
+                            class="form-control"
+                            name="email"
+                            value={parent.email}
+                            placeholder="Enter your parent email"
+                            onChange={handleChange}
+                            required
+                        />
+                        <div className="invalid-feedback">Please enter a valid email.</div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">
+                            <i className="fa fa-solid fa-phone icon" style={{ marginRight: '10px' }}></i>
+                        </span>
+                        <input
+                            type="tel"
+                            class="form-control"
+                            name="phone"
+                            pattern="[0-9]{10}"
+                            value={parent.phone}
+                            placeholder="Enter your parent phone number"
+                            onChange={handleChange}
+                            required
+                        />
+                        <div className="invalid-feedback">Please enter a phone number.</div>
+                    </div>
+
+                    <h4>Select Role</h4>
+                    <div class="input-group mb-3">
+                        <br />
+                        <div class="btn-group" role="group">
                             <input
-                                className="input_i"
+                                type="radio"
+                                class="btn-check"
+                                name="gender"
+                                value="father"
+                                id="btnradio1"
                                 onChange={handleChange}
-                                name="name"
-                                type="text"
-                                placeholder="Enter your parent name"
-                                value={parent.name}
-                                required
+                                checked={parent.gender === 'father'}
                             />
-                            <div className="input-icon">
-                                <i className="fa fa-user icon"></i>
-                            </div>
-                        </div>
-                        <div className="input-group input-group-icon">
+                            <label class="btn btn-outline-primary" for="btnradio1">
+                                Father
+                            </label>
+
                             <input
-                                className="input_i"
+                                type="radio"
+                                class="btn-check"
+                                name="gender"
+                                value="mother"
+                                id="btnradio2"
                                 onChange={handleChange}
-                                name="email"
-                                type="email"
-                                placeholder="Enter your parent email"
-                                value={parent.email}
-                                required
+                                checked={parent.gender === 'mother'}
                             />
-                            <div className="input-icon">
-                                <i className="fa fa-envelope"></i>
-                            </div>
-                        </div>
-                        <div className="input-group input-group-icon">
+                            <label class="btn btn-outline-primary" for="btnradio2">
+                                Mother
+                            </label>
+
                             <input
-                                className="input_i"
+                                type="radio"
+                                class="btn-check"
+                                name="gender"
+                                value="guardian"
+                                id="btnradio3"
                                 onChange={handleChange}
-                                value={parent.phone}
-                                name="phone"
-                                type="text"
-                                placeholder="Enter your parent phone number"
-                                required
+                                checked={parent.gender === 'guardian'}
                             />
-                            <div className="input-icon">
-                                <i className="fa fa-solid fa-phone icon"></i>
-                            </div>
+                            <label class="btn btn-outline-primary" for="btnradio3">
+                                Guardian
+                            </label>
                         </div>
                     </div>
-                    <div style={{ margin: '15px 0', marginLeft: '-10px' }} className="row">
-                        <div className="col-half">
-                            <h4>Gender</h4>
-                            <div className="input-group">
-                                <input
-                                    className="input_i"
-                                    onClick={handleChange}
-                                    id="gender-male"
-                                    type="radio"
-                                    name="gender"
-                                    value="male"
-                                />
-                                <label htmlFor="gender-male">Male</label>
-                                <input
-                                    className="input_i"
-                                    onClick={handleChange}
-                                    id="gender-female"
-                                    type="radio"
-                                    name="gender"
-                                    value="female"
-                                />
-                                <label htmlFor="gender-female">Female</label>
-                            </div>
-                        </div>
-                    </div>
-                    <button style={{ marginLeft: '1px' }} type="Sumbit" className="btn btn-primary btn-md">
+
+                    <button type="submit" className="btn btn-primary mt-3" style={{ width: 'fit-content' }}>
                         Submit
                     </button>
                 </form>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/fonts.css';
+import './styles/form.css';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
