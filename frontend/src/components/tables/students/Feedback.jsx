96d870ab55a7e762aac47b1f6f59c6e75915dea1
BlazerHeat <vghoghari82@gmail.com>
2023-03-29 19:14:13 +0530

    yhlkjhkl

--- frontend/src/components/tables/students/Feedback.jsx
+++ frontend/src/components/tables/students/Feedback.jsx
@@ -21,7 +21,6 @@ const Feedback = () => {
                                 </p>
                             </div>
 
-
                             <form class="px-4" action="">
                                 <p class="text-center"><strong>Your rating:</strong></p>
 
