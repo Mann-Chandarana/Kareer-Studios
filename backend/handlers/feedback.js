b41d5495ec9cb466396f927221eb3ce82aa59db9
BlazerHeat <vghoghari82@gmail.com>
2023-04-01 13:33:03 +0530

    Update

--- backend/handlers/feedback.js
+++ backend/handlers/feedback.js
@@ -1,6 +1,10 @@
 const db = require('../db');
 
 module.exports = {
+	getallfeedback: async (counsellor_id) => {
+		console.log('Hello');
+		return db.query('SELECT * FROM counsellors where id =$1', [counsellor_id]);
+	},
 	addStudentFeedback: async (student_id, counsellor_id, referal, rating, overall_exp) => {
 		return db.query(
 			'INSERT INTO student_feedbacks (student_id, counsellor_id, referral, rating, overall_experience) VALUES ($1, $2, $3, $4, $5)',
--- backend/handlers/feedbacks.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db')
-
-module.exports = {
-    getallfeedback: async (counsellor_id) => {
-        console.log("Hello")
-        return db.query('SELECT * FROM counsellors where id =$1',[counsellor_id])
-    },
-};
