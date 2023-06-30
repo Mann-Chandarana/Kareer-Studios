405a9def88a69b3e8c98a8644aa6f63e9209da25
BlazerHeat <vghoghari82@gmail.com>
2023-06-30 22:59:34 +0530

    update

--- backend/db/multer.js
+++ backend/db/multer.js
@@ -2,13 +2,13 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-  destination: path.resolve(__dirname, '../static/files'),
-  filename: (req, file, cb) => {
-    const filename = Date.now() + '-' + file.originalname;
-    const url = `http://localhost:8000/static/files/${filename}`;
-    req.body.fileurl = url;
-    cb(null, filename);
-  },
+    destination: path.resolve(__dirname, '../static/files'),
+    filename: (req, file, cb) => {
+        const filename = Date.now() + '-' + file.originalname;
+        const url = process.env.ENV === 'development' ? `http://localhost:8000/static/files/${filename}` : process.env.SERVER_URL + '/static/files/${filename}`';
+        req.body.fileurl = url;
+        cb(null, filename);
+    },
 });
 
 const upload = multer({ storage: storage });
