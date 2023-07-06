638ae5d5d19f06cf15231b7172fa1691d83aed42
BlazerHeat <vghoghari82@gmail.com>
2023-07-06 22:34:06 +0530

    Update loading screen

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -1,41 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import '../styles/loading.css';
 
 function Loading() {
-  const [dots, setDots] = useState('');
+    const [dots, setDots] = useState('');
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDots((prev) => {
-        if (prev === '...') {
-          return '';
-        }
-        return prev.concat('.');
-      });
-    }, 250);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDots((prev) => {
+                if (prev === '...') {
+                    return '';
+                }
+                return prev.concat('.');
+            });
+        }, 250);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-  return (
-    <div
-      className="w-100 d-flex flex-column"
-      style={{
-        height: '100vh',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <strong>{'Loading' + dots}</strong>
-
-      <div
-        className="spinner-border ml-auto text-primary"
-        role="status"
-        aria-hidden="true"
-      ></div>
-    </div>
-  );
+    return (
+        <div
+            className='w-100 d-flex flex-column'
+            style={{
+                height: '100vh',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <div class='lds-grid'>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
 }
 
 export default Loading;
