911ce00c76a2f4354d7232cc1b86aab56a03709f
BlazerHeat <vghoghari82@gmail.com>
2023-07-06 22:37:27 +0530

    fixed typo

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -1,24 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../styles/loading.css';
 
 function Loading() {
-    const [dots, setDots] = useState('');
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setDots((prev) => {
-                if (prev === '...') {
-                    return '';
-                }
-                return prev.concat('.');
-            });
-        }, 250);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-
     return (
         <div
             className='w-100 d-flex flex-column'
